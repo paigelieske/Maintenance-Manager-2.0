2a288be3bfb407f610b8f48dbff16713d25d9657
Kit <46611409+ankitbaraskar@users.noreply.github.com>
2019-07-10 20:23:35 -0400

    added user model

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    permission: {
+        type: [String],
+        enum: ['user','admin'],
+        default: ['user']
+    }
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const routes = require("./routes/api");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -10,6 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(routes);
+
+// connect to mongoose DB
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/maintenancemanager",
+    { useNewUrlParser: true }
+  );
 
 app.listen(PORT, () => {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
