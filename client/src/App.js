57255f830f170922e614256e4282cb62cff0fd7b
Taylor <Taylor@Taylors-MacBook-Pro.local>
2019-07-13 10:51:58 -0400

    changing some files up

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import EverythingContainer from './Components/EverythingContainer';
-import ContainerForTasks from './Components/ContainerForTasks'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import UserPage from './Pages/user'
 import NavBar from './Components/NavBar'
-// import StartButtonCreater from './Components/StartButton'
-import NewTaskDivAndButtons from './Components/NewTaskDivAndButtons'
 import Footer from './Components/Footer'
 import './App.css';
 
 function App() {
   return (
     <React.Fragment>
-      <NavBar></NavBar>
-      <EverythingContainer>
-        <ContainerForTasks>
-        </ContainerForTasks>
-
-        <NewTaskDivAndButtons/>
-      </EverythingContainer>
-      <Footer/>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={UserPage} />
+          <Route exact path="/users" component={UserPage} />
+          <Route exact path="/admin" component={UserPage} />
+          <Route component={UserPage} />
+        </Switch>
+        <Footer />
+      </Router>
     </React.Fragment>
   );
 }
