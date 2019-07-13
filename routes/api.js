867ba9ea570cacbc1e1ca142414572920d2c9884
Paige Lieske <paigeproctor@comcast.net>
2019-07-13 12:51:12 -0400

    correct branch from master

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,15 @@
 const apiRouter = require("express").Router();
+const tasksController = require("../controllers/tasksController");
 
-apiRouter.get("/", () => {
-    console.log("routes");
-})
+router
+  .route("/api/tasks")
+  .get(tasksController.findAll)
+  .post(tasksController.create);
+
+router
+  .route("api/tasks/:id")
+  .get(tasksController.findById)
+  .put(tasksController.update)
+  .delete(tasksController.remove);
 
 module.exports = apiRouter;
