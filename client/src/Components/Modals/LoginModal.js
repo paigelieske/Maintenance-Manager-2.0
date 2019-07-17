3b2835dfc4ffaae917958c2b0bdfd8914e482069
Kit <46611409+ankitbaraskar@users.noreply.github.com>
2019-07-16 20:14:01 -0400

    some formatting in login modal and signup modal

--- client/src/Components/Modals/LoginModal.js
+++ client/src/Components/Modals/LoginModal.js
@@ -4,58 +4,58 @@ import Modal from 'react-bootstrap/Modal';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 
 function MyVerticallyCenteredModal(props) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Sign In
+	return (
+		<Modal
+			{...props}
+			size="lg"
+			aria-labelledby="contained-modal-title-vcenter"
+			centered
+		>
+			<Modal.Header closeButton>
+				<Modal.Title id="contained-modal-title-vcenter">
+					Sign In
         </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <form
-							action="/login"
-							method="post"
-							className="passport-form"
-						>
-							<div className="username">
-								<label>Username:</label>
-								<input
-									type="text"
-									name="username"
-									id="username-form"
-								/>
-							</div>
-							<div className="password">
-								<label>Password:</label>
-								<input
-									type="password"
-									name="password"
-									id="password-form"
-								/>
-							</div>
-							<div id="login-btn">
-								<input
-									type="submit"
-									value="Log In"
-									id="submit-form"
-								/>
-							</div>
-						</form>
-      </Modal.Body>
+			</Modal.Header>
+			<Modal.Body>
+				<form
+					action="/login"
+					method="post"
+					className="passport-form"
+				>
+					<div className="username">
+						<label>Username:</label>
+						<input
+							type="text"
+							name="username"
+							id="username-form"
+						/>
+					</div>
+					<div className="password">
+						<label>Password:</label>
+						<input
+							type="password"
+							name="password"
+							id="password-form"
+						/>
+					</div>
+					<div id="login-btn">
+						<input
+							type="submit"
+							value="Log In"
+							id="submit-form"
+						/>
+					</div>
+				</form>
+			</Modal.Body>
 
-    </Modal>
-  );
+		</Modal>
+	);
 }
 
 function LoginModal() {
-  const [modalShow, setModalShow] = React.useState(false);
+	const [modalShow, setModalShow] = React.useState(false);
 
-  return (
+	return (
 		<ButtonToolbar>
 			<Button
 				className="login-button"
@@ -71,7 +71,7 @@ function LoginModal() {
 				onHide={() => setModalShow(false)}
 			/>
 		</ButtonToolbar>
-  );
+	);
 }
 
 export default LoginModal;
--- client/src/Components/Modals/SignupModal.js
+++ client/src/Components/Modals/SignupModal.js
@@ -4,57 +4,57 @@ import Modal from 'react-bootstrap/Modal';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 
 function MyVerticallyCenteredModal(props) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Register
+	return (
+		<Modal
+			{...props}
+			size="lg"
+			aria-labelledby="contained-modal-title-vcenter"
+			centered
+		>
+			<Modal.Header closeButton>
+				<Modal.Title id="contained-modal-title-vcenter">
+					Register
         </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <form
-							action="/login"
-							method="post"
-							className="passport-form"
-						>
-							<div className="username">
-								<label>Username:</label>
-								<input
-									type="text"
-									name="username"
-									id="username-form"
-								/>
-							</div>
-							<div className="password">
-								<label>Password:</label>
-								<input
-									type="password"
-									name="password"
-									id="password-form"
-								/>
-							</div>
-							<div id="login-btn">
-								<input
-									type="submit"
-									value="Register"
-									id="submit-form"
-								/>
-							</div>
-						</form>
-      </Modal.Body>
-    </Modal>
-  );
+			</Modal.Header>
+			<Modal.Body>
+				<form
+					action="/login"
+					method="post"
+					className="passport-form"
+				>
+					<div className="username">
+						<label>Username:</label>
+						<input
+							type="text"
+							name="username"
+							id="username-form"
+						/>
+					</div>
+					<div className="password">
+						<label>Password:</label>
+						<input
+							type="password"
+							name="password"
+							id="password-form"
+						/>
+					</div>
+					<div id="login-btn">
+						<input
+							type="submit"
+							value="Register"
+							id="submit-form"
+						/>
+					</div>
+				</form>
+			</Modal.Body>
+		</Modal>
+	);
 }
 
 function SignupModal() {
-  const [modalShow, setModalShow] = React.useState(false);
+	const [modalShow, setModalShow] = React.useState(false);
 
-  return (
+	return (
 		<ButtonToolbar>
 			<Button
 				className="signup-button"
@@ -69,7 +69,7 @@ function SignupModal() {
 				onHide={() => setModalShow(false)}
 			/>
 		</ButtonToolbar>
-  );
+	);
 }
 
 export default SignupModal;
